09bc9377c71918f81d0b09c9ff7e98d0608845e2
ririgireddy <ririgireddy1@gmail.com>
2022-07-03 01:11:26 -0700

    add confetti & styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { useReward } from 'react-rewards';
 
 // Import Swiper styles
 import "swiper/css";
@@ -10,16 +11,18 @@ import "swiper/css/effect-cards";
 import { EffectCards } from "swiper";
 import data from "./data";
 
-import './App.css';
-
-
+import "./App.css";
 
 function App() {
 
+  const { reward, isAnimating } = useReward('rewardId', 'confetti');
 
 
-
-
+  function click(event) {
+    var val = event.target.value;
+    var newVal = val.split(",");
+    var bool = newVal[1];
+  }
 
   return (
     <div>
@@ -27,21 +30,35 @@ function App() {
         effect={"cards"}
         grabCursor={true}
         modules={[EffectCards]}
-        className="mySwiper"
+        className=" mt-40"
       >
 
         {data.map((item, index) => (
           <SwiperSlide>
             <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">{item.question}</h5>
-                {item.answers.map((answer) => {
-              return (
-                <div >
-                  <h2>Answer: {answer}</h2>
+              <div className=" px-10 card-body">
+                <div className=" pt-20 pb-10">
+                  <h5 className="card-title text-xl font-semibold">
+                    {item.question}
+                  </h5>
                 </div>
-              );
-            })}
+                {item.answers.map((answer) => {
+                  return (
+                    <div className="flex justify-center">
+                      <h2 className="font-medium text-lg">
+                        <button
+                          onClick={click}
+                          class="btn btn-wide btn-active btn-ghost"
+                          value={answer}
+                          disabled={isAnimating}
+                          id="rewardId"
+                        >
+                          {answer}
+                        </button>
+                      </h2>
+                    </div>
+                  );
+                })}
               </div>
             </div>
           </SwiperSlide>
@@ -50,6 +67,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
